0cfeb2653226e01bd9ca08b831f6c13a5c389291
densaintp <73133105+densaintp@users.noreply.github.com>
2022-09-06 20:13:23 +0300

    Добавил выгрузку результата в текстовый файл.

--- UniqueWords/Program.cs
+++ UniqueWords/Program.cs
@@ -1,22 +1,24 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
 	static void Main()
 	{
-		Console.WriteLine("Укажите путь сохранения txt-файла: ");
-		string filePath = Console.ReadLine();	
+		Console.WriteLine("Укажите путь к txt-файлу: ");
+		string filePath = Console.ReadLine();
 
 		if (File.Exists(filePath))
 		{
 			// Читаем файл построчно
-			var textFile = new StreamReader(filePath);
+			var readTextFile = new StreamReader(filePath);
 			int rowsCounter = 0;
 			int wordsCounter = 1;
 
 			var dict = new Dictionary<string, int>();
 
-			while (!textFile.EndOfStream)
+			while (!readTextFile.EndOfStream)
 			{
-				string[] line = textFile.ReadLine().ToLower().Split(" ,.?!\'()\"".ToCharArray(), StringSplitOptions.RemoveEmptyEntries); 
+				string[] line = readTextFile.ReadLine().ToLower().Split(" ,.[]--{}?!\'()\"".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 				rowsCounter++;
 				foreach (var word in line)
 				{
@@ -26,17 +28,32 @@
 						dict[word]++;
 				}
 			}
+
 			var result = dict.OrderByDescending(x => x.Value);
 
-			foreach (var keyValuePair in result)
+			if (readTextFile.EndOfStream)
 			{
-				Console.WriteLine("Слово {0} встречается {1} раз", keyValuePair.Key, keyValuePair.Value);
+				Console.WriteLine("Введите место сохранения файла");
+				string path = Console.ReadLine() + @"\test.txt";
+				FileStream file = new FileStream(path, FileMode.Append);
+				StreamWriter fnew = new StreamWriter(file, Encoding.UTF8);
+				rowsCounter++;
+				foreach (var keyValuePair in result)
+				{
+					fnew.WriteLine("Слово {0} встречается {1} раз", keyValuePair.Key, keyValuePair.Value);
+
+				}
 			}
 		}
+
 		else
 		{
 			Console.WriteLine("По данному пути ничего не найдено");
 		}
+
+		Console.WriteLine("Нажмите на любую кнопку для выхода...");
+		Console.ReadKey(true);
 	}
+
 }
 
